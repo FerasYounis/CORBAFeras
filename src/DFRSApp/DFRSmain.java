4d0c3a0a0ec57380774ae60616695d1735e0b866
Feras Younis <f_youni@encs.concordia.ca>
2016-12-04 22:44:35 -0500

    make the project run

--- src/DFRSApp/DFRSmain.java
+++ src/DFRSApp/DFRSmain.java
@@ -8,18 +8,7 @@ import java.util.Scanner;
 
 public class DFRSmain {
 
-	public void initServer(ORB orb, String[] args, String serverName) throws InvalidName, AdapterInactive,   ServantNotActive, WrongPolicy, org.omg.CosNaming.NamingContextPackage.InvalidName, NotFound, CannotProceed {   
-		  POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));  
-		  // activate the POAManager  
-		  rootpoa.the_POAManager().activate();  this.setORB(orb);  
-		  // get object reference from the servant  
-		  org.omg.CORBA.Object ref = rootpoa.servant_to_reference(this);  
-		  FlightServerInterface href = FlightServerInterfaceHelper.narrow(ref);  
-		  org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");  
-		  NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);  
-		  NameComponent path[] = ncRef.to_name(serverName);  ncRef.rebind(path, href);  
-		  this.serverLocation = serverName;  System.out.println("Corba: '" + serverLocation + "' server is up and running"); 
-		  }
+
 	
     static void threadMessage(String message) {
         String threadName =
